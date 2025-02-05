fc77604bbc790493eaa1e73679d693c3e620bad2
Andy Castle <masterchief_and@live.com>
2025-02-05 10:32:44 -0500

    Se implemento la funcionalidad del guard, osea este necesita logearse para poder entrar al dashboard, pero todavia la sesion no es persistente

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -1,8 +1,24 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AuthStatus } from '../interfaces';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
-  
-  console.log('isAuthenticatedGuard', { route, state });
+  // const url = state.url;
+  // localStorage.setItem('url', url);
+  // console.log('isAuthenticatedGuard', { route, state });
+  const authService = inject(AuthService);
 
-  return true;
+  // console.log({ status: authService.authStatus() });
+
+  if (authService.authStatus() == AuthStatus.authenticated) {
+    return true;
+  }
+
+  const router = inject(Router);
+  // const url = state.url;
+  // localStorage.setItem('url', url);
+  router.navigateByUrl('/auth/login');
+
+  return false;
 };
