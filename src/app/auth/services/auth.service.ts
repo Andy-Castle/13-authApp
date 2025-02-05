09ce0306a4c287d8e06137ad448387bf0f733759
Andy Castle <masterchief_and@live.com>
2025-02-05 10:52:36 -0500

    se aplico el DRY para el setAuthentication

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -26,19 +26,27 @@ export class AuthService {
 
   constructor() {}
 
+  private setAuthentication(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+    return true;
+  }
+
   login(email: string, password: string): Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
 
     const body = { email, password };
 
     return this.http.post<LoginResponse>(url, body).pipe(
-      tap(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-        // console.log({ user, token });
-      }),
-      map(() => true),
+      // tap(({ user, token }) => {
+      //   this._currentUser.set(user);
+      //   this._authStatus.set(AuthStatus.authenticated);
+      //   localStorage.setItem('token', token);
+      //   // console.log({ user, token });
+      // }),
+      // map(() => true),
+      map(({ user, token }) => this.setAuthentication(user, token)),
 
       //Todo: errores
       // catchError((err) => {
@@ -61,12 +69,13 @@ export class AuthService {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
-      map(({ token, user }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-        return true;
-      }),
+      // map(({ token, user }) => {
+      //   this._currentUser.set(user);
+      //   this._authStatus.set(AuthStatus.authenticated);
+      //   localStorage.setItem('token', token);
+      //   return true;
+      // }),
+      map(({ user, token }) => this.setAuthentication(user, token)),
       //error
       catchError(() => {
         this._authStatus.set(AuthStatus.notAuthenticated);
